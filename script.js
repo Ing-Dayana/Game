33b3e7cb0b414aefaebc0f2ca0d6ab39cdcb58d7
Dayana Martinez <110215625+Ing-Dayana@users.noreply.github.com>
2023-02-09 23:31:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+const options = document.querySelectorAll(".options i");
+const score = document.querySelector("#score");
+const result = document.querySelector("#result");
+const lineVertical = document.querySelector(".line-vertical");
+const resultTotal = document.querySelector(".result-total");
+const userChoiceText = document.getElementById("user-choice-text");
+const computerChoiceText = document.getElementById("computer-choice-text");
+
+
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+options.forEach(option => {
+    option.addEventListener("click", function() {
+        // Generar una opción aleatoria por la computadora
+        const computerChoice = Math.floor(Math.random() * 3);
+        const computerOption = options[computerChoice];
+        
+        // Determinar el resultado final
+        let message;
+        if (this.classList[1] === "fa-hand-rock" && computerOption.classList[1] === "fa-hand-scissors") {
+        message = "Ganaste";
+        playerScore++;
+        } else if (this.classList[1] === "fa-hand-paper" && computerOption.classList[1] === "fa-hand-rock") {
+        message = "Ganaste";
+        playerScore++;
+        } else if (this.classList[1] === "fa-hand-scissors" && computerOption.classList[1] === "fa-hand-paper") {
+        message = "Ganaste";
+        playerScore++;
+        } else if (this.classList[1] === computerOption.classList[1]) {
+        message = "Empate";
+        } else {
+        message = "Perdiste";
+        computerScore++;
+        }
+        
+        // Determinar el texto de la opción elegida por el usuario
+        let userSelection;
+        if (this.classList[1] === "fa-hand-rock") {
+            userSelection = "rock";
+        } else if (this.classList[1] === "fa-hand-paper") {
+            userSelection = "paper";
+        } else if (this.classList[1] === "fa-hand-scissors") {
+            userSelection = "scissors";
+        }
+         // Determinar el texto de la opción elegida por la computadora
+        let computerSelection;
+        if (computerOption.classList[1] === "fa-hand-rock") {
+            computerSelection = "rock";
+        } else if (computerOption.classList[1] === "fa-hand-paper") {
+            computerSelection = "paper";
+        } else if (computerOption.classList[1] === "fa-hand-scissors") {
+            computerSelection = "scissors";
+        }
+
+        userChoiceText.textContent = userSelection === "rock" ? "Roca" : userSelection === "paper" ? "Papel" : "Tijeras";
+        computerChoiceText.textContent = computerSelection === "rock" ? "Roca" : computerSelection === "paper" ? "Papel" : "Tijeras";
+        
+        // Mostrar el resultado
+        result.textContent = message;
+        score.textContent = `${playerScore} - ${computerScore}`;
+        
+        // Actualizar los iconos
+        document.querySelector("#user-choice").classList = this.classList;
+        document.querySelector("#computer-choice").classList = computerOption.classList;
+
+        roundsPlayed++;
+
+        // Reiniciar el juego después de tres partidas
+        if (roundsPlayed === 4) {
+        playerScore = 0;
+        computerScore = 0;
+        roundsPlayed = 0;
+        score.textContent = `${playerScore} - ${computerScore}`;
+        result.textContent = "";
+        document.querySelector("#user-choice").classList = "";
+        document.querySelector("#computer-choice").classList = "";
+        result.textContent = "";
+        }
+
+        // Ocultar Clases para mensaje ganador/perdedor
+        if (message) {
+        result.textContent = "";
+        lineVertical.style.display = "block";
+        resultTotal.style.display = "flex";
+
+        setTimeout(function() {
+            result.textContent = message;
+            lineVertical.style.display = "none";
+            resultTotal.style.display = "none";
+        }, 1000);
+        }
+        setTimeout(function() {
+        lineVertical.style.display = "block";
+        resultTotal.style.display = "none";
+        result.textContent = "";
+        }, 2000);
+    });
+});
+
+
+
+
